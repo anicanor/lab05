6bc013be6551523f9f587ed9e0234e2159a69170
anicanor <31227133+anicanor@users.noreply.github.com>
2018-02-24 21:53:15 -0800

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -43,6 +43,15 @@ void List::print()
 void List::compare(int target, int &less_than, int &equal, int &greater_than)
 {
 
-    // put your code here 
+    While(m_head != NULL){
+            
+        if(target > m_head->m_data){
+            greater_than++;
+        }else if(target < m_head->m_data){
+            less_than++;
+        }else{
+            equal++;
+        }
+    }
 
 }
